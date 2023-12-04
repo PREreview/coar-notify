6e9f64ab93035e10383d8e6e2708fb89164227da
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-12-04 14:34:45 +0000

    Run an empty HTTP server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-import { Effect } from 'effect'
+import { HttpServer, Runtime } from '@effect/platform-node'
+import { Effect, Layer } from 'effect'
+import { createServer } from 'node:http'
 
-await Effect.runPromise(Effect.logInfo('hello'))
+const serve = HttpServer.router.empty.pipe(
+  Effect.catchTag('RouteNotFound', () => HttpServer.response.empty({ status: 404 })),
+  HttpServer.server.serve(HttpServer.middleware.logger),
+)
+
+const ServerLive = HttpServer.server.layer(() => createServer(), { port: 3000 })
+
+const HttpLive = Layer.scopedDiscard(serve).pipe(Layer.provide(ServerLive))
+
+Layer.launch(HttpLive).pipe(Effect.tapErrorCause(Effect.logError), Runtime.runMain)
