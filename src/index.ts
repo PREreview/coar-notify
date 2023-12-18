a843693fd79d003dffbceada5285a7e0e76fdbf0
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-12-18 11:28:50 +0000

    Extract the router into its own module

--- src/index.ts
+++ src/index.ts
@@ -1,128 +1,15 @@
 import { HttpClient, HttpServer, Runtime } from '@effect/platform-node'
-import { Schema, TreeFormatter } from '@effect/schema'
 import { Effect, Layer, LogLevel, Logger } from 'effect'
 import { createServer } from 'node:http'
-import * as CoarNotify from './CoarNotify.js'
 import { ConfigLive } from './Config.js'
-import * as Doi from './Doi.js'
 import * as Redis from './Redis.js'
-import * as Slack from './Slack.js'
-import * as Temporal from './Temporal.js'
-
-const NotificationSchema = Schema.struct({
-  timestamp: Temporal.InstantInMillisecondsSchema,
-  notification: CoarNotify.ReviewActionSchema,
-})
-
-const serve = HttpServer.router.empty.pipe(
-  HttpServer.router.get(
-    '/health',
-    Effect.gen(function* (_) {
-      yield* _(Redis.ping())
-
-      return yield* _(HttpServer.response.json({ status: 'ok' }), HttpServer.middleware.withLoggerDisabled)
-    }).pipe(
-      Effect.catchTags({
-        RedisError: error =>
-          Effect.gen(function* (_) {
-            yield* _(Effect.logError('Unable to ping Redis').pipe(Effect.annotateLogs({ message: error.message })))
-
-            return yield* _(HttpServer.response.json({ status: 'error' }, { status: 503 }))
-          }),
-      }),
-    ),
-  ),
-  HttpServer.router.post(
-    '/inbox',
-    Effect.gen(function* (_) {
-      const timestamp = yield* _(Temporal.Timestamp)
-      const reviewAction = yield* _(HttpServer.request.schemaBodyJson(CoarNotify.ReviewActionSchema))
-
-      const encoded = yield* _(
-        Schema.encode(Schema.ParseJson.pipe(Schema.compose(NotificationSchema)))({
-          timestamp,
-          notification: reviewAction,
-        }),
-      )
-
-      yield* _(Redis.lpush('notifications', encoded))
-
-      yield* _(
-        Slack.chatPostMessage({
-          channel: 'C05B95LEN5C',
-          blocks: [
-            {
-              type: 'section',
-              text: {
-                type: 'mrkdwn',
-                text: `A new request from ${reviewAction.actor.name} has come in for a review of <${
-                  Doi.toUrl(reviewAction.object['ietf:cite-as']).href
-                }|${reviewAction.object['ietf:cite-as']}>`,
-              },
-              accessory: {
-                type: 'button',
-                text: {
-                  type: 'plain_text',
-                  text: 'Write a PREreview',
-                },
-                url: new URL(
-                  `https://prereview.org/preprints/doi-${reviewAction.object['ietf:cite-as']
-                    .toLowerCase()
-                    .replaceAll('-', '+')
-                    .replaceAll('/', '-')}/write-a-prereview`,
-                ),
-              },
-            },
-          ],
-        }),
-      )
-
-      return yield* _(HttpServer.response.empty({ status: 201 }))
-    }).pipe(
-      Effect.catchTags({
-        ParseError: error =>
-          Effect.gen(function* (_) {
-            yield* _(
-              Effect.logInfo('Invalid request').pipe(
-                Effect.annotateLogs({ message: TreeFormatter.formatErrors(error.errors) }),
-              ),
-            )
-
-            return HttpServer.response.empty({ status: 400 })
-          }),
-        RedisError: error =>
-          Effect.gen(function* (_) {
-            yield* _(
-              Effect.logError('Unable to write notification to Redis').pipe(
-                Effect.annotateLogs({ message: error.message }),
-              ),
-            )
-
-            return HttpServer.response.empty({ status: 503 })
-          }),
-        RequestError: () => HttpServer.response.empty({ status: 400 }),
-        SlackErrorResponse: error =>
-          Effect.gen(function* (_) {
-            yield* _(
-              Effect.logError('Unable post chat message on Slack').pipe(
-                Effect.annotateLogs({ message: error.message }),
-              ),
-            )
-
-            return HttpServer.response.empty({ status: 503 })
-          }),
-      }),
-    ),
-  ),
-  Effect.catchTag('RouteNotFound', () => HttpServer.response.empty({ status: 404 })),
-  HttpServer.server.serve(HttpServer.middleware.logger),
-)
+import { Router } from './Router.js'
 
 const ServerLive = HttpServer.server.layer(() => createServer(), { port: 3000 })
 
 const RedisLive = Redis.layer
 
-const HttpLive = Layer.scopedDiscard(serve).pipe(
+const HttpLive = Layer.scopedDiscard(Router).pipe(
   Layer.provide(Layer.mergeAll(HttpClient.client.layer, ServerLive, RedisLive)),
   Layer.provide(ConfigLive),
 )
