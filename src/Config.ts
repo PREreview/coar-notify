62f56766f157efaa06db43c50e9e3b08ec8d682b
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-12-20 10:18:33 +0000

    Make types more explicit

--- src/Config.ts
+++ src/Config.ts
@@ -3,17 +3,17 @@ import { RedisConfig } from './Redis.js'
 import { SlackChannelConfig } from './Router.js'
 import { SlackApiConfig } from './Slack.js'
 
-const slackApiConfig = Config.nested(
-  Config.map(Config.string('ACCESS_TOKEN'), accessToken => ({ accessToken }) satisfies SlackApiConfig),
+const slackApiConfig: Config.Config<SlackApiConfig> = Config.nested(
+  Config.map(Config.string('ACCESS_TOKEN'), accessToken => ({ accessToken })),
   'SLACK',
 )
 
-const slackChannelConfig = Config.nested(
-  Config.map(Config.string('CHANNEL_ID'), id => ({ id }) satisfies SlackChannelConfig),
+const slackChannelConfig: Config.Config<SlackChannelConfig> = Config.nested(
+  Config.map(Config.string('CHANNEL_ID'), id => ({ id })),
   'SLACK',
 )
 
-const redisConfig = Config.nested(
+const redisConfig: Config.Config<RedisConfig> = Config.nested(
   Config.all({
     url: Config.mapAttempt(Config.string('URL'), url => new URL(url)),
     family: Config.integer('IP_VERSION').pipe(Config.withDefault(4)),
