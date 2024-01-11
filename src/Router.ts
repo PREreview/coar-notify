79af3429b6bea64fed1b937227416f2587e703d0
Chris Wilkinson <c.wilkinson@elifesciences.org>
2024-01-11 11:23:26 +0000

    Send an HTML version of the email
    Refs #47

--- src/Router.ts
+++ src/Router.ts
@@ -2,6 +2,7 @@ import { HttpServer } from '@effect/platform-node'
 import { Schema, TreeFormatter } from '@effect/schema'
 import { Context, Data, Effect } from 'effect'
 import { StatusCodes } from 'http-status-codes'
+import mjml from 'mjml'
 import * as CoarNotify from './CoarNotify.js'
 import * as Doi from './Doi.js'
 import * as Nodemailer from './Nodemailer.js'
@@ -99,6 +100,51 @@ export const Router = HttpServer.router.empty.pipe(
             from: { name: 'PREreview', address: 'help@prereview.org' },
             to: { name: requestReview.actor.name, address: requestReview.actor.id.pathname },
             subject: 'Review requested from the PREreview community',
+            html: mjml(`
+              <mjml>
+                <mj-body>
+                  <mj-section>
+                    <mj-column>
+                      <mj-text>Hi ${requestReview.actor.name},</mj-text>
+                      <mj-text>Thank you for requesting a review from PREreview.</mj-text>
+                      <mj-text>
+                        While we cannot guarantee a review, we’ve shared your request with our PREreview Slack
+                        community.
+                      </mj-text>
+                      <mj-text>
+                        You can join our Slack Community and check out the #request-a-review channel by signing up at
+                        <a href="https://bit.ly/PREreview-Slack">bit.ly/PREreview-Slack</a>.
+                      </mj-text>
+                      <mj-text>
+                        If you have any questions, please let us know at
+                        <a href="mailto:help@prereview.org">help@prereview.org</a>.
+                      </mj-text>
+                      <mj-text>All the best,<br />PREreview</mj-text>
+                    </mj-column>
+                  </mj-section>
+                  <mj-section padding-bottom="0" border-top="1px solid lightgrey">
+                    <mj-column width="25%" vertical-align="middle">
+                      <mj-image
+                        href="https://prereview.org"
+                        src="https://res.cloudinary.com/prereview/image/upload/f_auto,q_auto,w_300/v1704970584/emails/logo_tbhi5b"
+                        padding="0"
+                      />
+                    </mj-column>
+                    <mj-column width="75%" vertical-align="middle">
+                      <mj-text font-size="11px">PREreview is a platform, resource center, and convener.</mj-text>
+                      <mj-text font-size="11px">
+                        We provide ways for feedback to preprints to be done openly, rapidly, constructively, and by a
+                        global community of peers.
+                      </mj-text>
+                      <mj-text font-size="11px">
+                        Join us at <a href="https://prereview.org">prereview.org</a> and
+                        <a href="https://bit.ly/PREreview-Slack">sign up to our vibrant Slack community</a>.
+                      </mj-text>
+                    </mj-column>
+                  </mj-section>
+                </mj-body>
+              </mjml>
+            `).html,
             text: `
 Hi ${requestReview.actor.name},
 
