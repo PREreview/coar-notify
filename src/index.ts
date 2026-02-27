b060ac03fa79c99a9e314982070653defda5183c
Chris Wilkinson <chris@prereview.org>
2026-02-27 15:55:30 +0000

    Turn off the redundant queue worker
    Refs https://github.com/PREreview/prereview.org/issues/2891, 70753797dba9067dce106ee4a8e6065145cb58f1

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,8 @@ import {
   HttpServerRequest,
 } from '@effect/platform'
 import { NodeHttpServer, NodeRuntime } from '@effect/platform-node'
-import { Config, Effect, Layer, LogLevel, Logger, Option, Request, Schedule, Schema } from 'effect'
+import { Config, Effect, Layer, LogLevel, Logger, Option, Request } from 'effect'
 import { createServer } from 'node:http'
-import * as BullMq from './BullMq.js'
-import * as CoarNotify from './CoarNotify.js'
 import { ConfigLive } from './Config.js'
 import * as Crossref from './Crossref.js'
 import * as Datacite from './Datacite.js'
@@ -20,7 +18,6 @@ import * as Nodemailer from './Nodemailer.js'
 import { OpenAi } from './OpenAi.js'
 import * as OpenAlex from './OpenAlex/index.js'
 import * as Redis from './Redis.js'
-import * as ReviewRequest from './ReviewRequest.js'
 import { PublicUrl, Router } from './Router.js'
 
 const logRequest = HttpMiddleware.make(app =>
@@ -71,42 +68,8 @@ const HttpClientLive = Layer.effect(
 
 const RedisLive = Redis.layer
 
-const QueueWorkerLive = Layer.effectDiscard(
-  Config.withDefault(Config.duration('BULLMQ_WORKER_POLL'), '10 seconds').pipe(
-    Effect.flatMap(schedule =>
-      BullMq.run(
-        'coar-notify',
-        data =>
-          Effect.gen(function* () {
-            const requestReview = yield* Schema.decodeUnknown(CoarNotify.RequestReviewSchema)(data)
-
-            yield* ReviewRequest.handleReviewRequest(requestReview)
-          }).pipe(
-            Effect.catchTag('PreprintNotReady', () => Effect.fail(new BullMq.DelayedJob({ delay: '10 minutes' }))),
-          ),
-        Schedule.spaced(schedule),
-      ),
-    ),
-  ),
-)
-
-export const NotificationsQueueLive = BullMq.makeLayer<
-  'coar-notify',
-  { 'request-review': Schema.Schema.Encoded<typeof CoarNotify.RequestReviewSchema> }
->({
-  name: 'coar-notify',
-  defaultJobOptions: { delay: '10 seconds', removeOnComplete: true, removeOnFail: false },
-})
-
-const Program = Layer.mergeAll(ServerLive, QueueWorkerLive).pipe(
-  Layer.provide(
-    Layer.mergeAll(
-      NotificationsQueueLive,
-      Crossref.CrossrefApiLive,
-      Datacite.DataciteApiLive,
-      OpenAlex.OpenAlexApiLive,
-    ),
-  ),
+const Program = ServerLive.pipe(
+  Layer.provide(Layer.mergeAll(Crossref.CrossrefApiLive, Datacite.DataciteApiLive, OpenAlex.OpenAlexApiLive)),
   Layer.provide(Layer.mergeAll(OpenAi.Live, HttpClientLive, RedisLive, Nodemailer.layer)),
   Layer.provide(ConfigLive),
   Layer.provide(Logger.json),
